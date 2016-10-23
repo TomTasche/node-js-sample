3bbee340a2274dffa4a733bfdd74ab38c8530eaf
Thomas Taschauer <tomtasche@gmail.com>
2016-10-23 13:57:02 +0200

    wait for all promises to settle, then exit

--- index.js
+++ index.js
@@ -5,67 +5,94 @@ var mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI);
 
 var trackSchema = {
-    name: String,
-    title: String,
-    artist: String,
-    station: String
+  name: String,
+  title: String,
+  artist: String,
+  station: String
 };
 
 var Track = mongoose.model("Track", trackSchema);
 
 function saveTrack(name, title, artist, station) {
-    var future = deferred();
+  var future = deferred();
 
-    var track = new Track();
-    track.name = name;
-    track.title = title;
-    track.artist = artist;
-    track.station = station;
+  var track = new Track();
+  track.name = name;
+  track.title = title;
+  track.artist = artist;
+  track.station = station;
 
-    console.log("saving track: " + JSON.stringify(track));
+  console.log("saving track: " + JSON.stringify(track));
 
-    track.save(function(error) {
-        if (error) {
-            console.log(error);
+  track.save(function(error) {
+    if (error) {
+      console.log(error);
 
-            future.reject(error);
-        } else {
-            console.log("saved track " + track.name + " from station " + track.station);
+      future.reject(error);
+    } else {
+      console.log("saved track " + track.name + " from station " + track.station);
 
-            future.resolve();
-        }
+      future.resolve();
+    }
+  });
+
+  return future.promise;
+}
+
+function waitFor(promises) {
+  var future = deferred();
+
+  var countdown = promises.length;
+
+  for (var i = 0; i < promises.length; i++) {
+    promises[i].then(function() {
+      countdown--;
+
+      if (countdown === 0) {
+        future.resolve();
+      }
     });
+  }
 
-    return future.promise;
+  return future.promise;
 }
 
 function fetchRadioOe3() {
-    var future = deferred();
+  var future = deferred();
 
-    var requestOptions = {
-        url: "http://oe3meta.orf.at/oe3mdata/WebPlayerFiles/PlayList200.json",
-        json: true
-    };
+  var requestOptions = {
+    url: "http://oe3meta.orf.at/oe3mdata/WebPlayerFiles/PlayList200.json",
+    json: true
+  };
 
-    request(requestOptions, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            for (var i = 0; i < body.length; i++) {
-                var trackData = body[i];
+  request(requestOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var trackPromises = [];
+      for (var i = 0; i < body.length; i++) {
+        var trackData = body[i];
 
-                var title = trackData["SongName"];
-                var artist = trackData["Artist"];
-                var name = artist + " - " + title;
-                var station = "OE3";
+        var title = trackData["SongName"];
+        var artist = trackData["Artist"];
+        var name = artist + " - " + title;
+        var station = "OE3";
 
-                // TODO: wait for tracks to be saved
-                saveTrack(name, title, artist, station);
-            }
-        } else {
-            future.reject(error);
-        }
-    });
+        var trackPromise = saveTrack(name, title, artist, station);
+        trackPromises.push(trackPromise);
+      }
 
-    return future.promise;
+      var savePromise = waitFor(trackPromises);
+      future.resolve(savePromise);
+    } else {
+      future.reject(error);
+    }
+  });
+
+  return future.promise;
 }
 
-fetchRadioOe3();
+var oe3Promise = fetchRadioOe3();
+oe3Promise.then(function() {
+  console.log("exitting!");
+
+  process.exit(0);
+});
